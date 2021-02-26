3642edbc632375a18e5816b9ffd47f530bcf619b
Yusuke KUWAHARA <barabaaaara0127@gmail.com>
2021-02-27 08:56:13 +0900

    second commit

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import Firebase
+import GoogleMaps
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var textFiled: UITextField!
+    var locationManager = CLLocationManager()
+    lazy var mapView = GMSMapView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //初期値はApple本社
+        let camera = GMSCameraPosition.camera(withLatitude: 37.3318, longitude: -122.0312, zoom: 20.0)
+        mapView = GMSMapView.map(withFrame: CGRect(origin: .zero, size: view.bounds.size), camera: camera)
+        mapView.settings.myLocationButton = true //右下のボタン追加する
+        mapView.isMyLocationEnabled = true
+
+        // User Location
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+
+        self.view.addSubview(mapView)
+        self.view.bringSubviewToFront(mapView)
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations.last
+        
+        let camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude,
+                                              longitude: userLocation!.coordinate.latitude, zoom: 17.0)
+        self.mapView.animate(to: camera)
+        
+        locationManager.stopUpdatingLocation()
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        let db = Firestore.firestore()
+        
+        db.collection("map").document("79qzrWdUkhvI84Nd8BfQ").delete() { err in
+            if let err = err {
+                print("Error removing document: \(err)")
+            } else {
+                print("Document successfully removed!")
+            }
+        }
+        
+        
+    }
+    
 }
+
 
