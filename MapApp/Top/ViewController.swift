508bb8653e5363fd1c77750c3b576be52f20ec64
Yuyahase <haseyuy@gmail.com>
2021-04-13 20:40:40 +0900

    Returns a layout anchor with the specified inset by a fractional value.

--- MapApp/Top/ViewController.swift
+++ MapApp/Top/ViewController.swift
@@ -113,8 +113,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
     }
     
     func generateCluster(MapModels:[MapModel]){
-        print(MapModels.count)
-//        print(MapModels[0].storeName)
         MapModels.forEach { (Mapmodel) in
             let item = POIItem(position: CLLocationCoordinate2DMake(Mapmodel.coordinate?.latitude ?? 0, Mapmodel.coordinate?.longitude ?? 0), storeName: Mapmodel.storeName, smokingSpace: Mapmodel.smokingSpace, openHour: Mapmodel.openHour, closeHour: Mapmodel.closeHour, tel: Mapmodel.tel)
             clusterManager.add(item)
@@ -170,7 +168,7 @@ class MyFloatingPanelLayout: FloatingPanelLayout {
     let position: FloatingPanelPosition = .bottom
     let initialState: FloatingPanelState = .tip
     var anchors: [FloatingPanelState: FloatingPanelLayoutAnchoring] {
-        return [.tip: FloatingPanelLayoutAnchor(absoluteInset: 90.0, edge: .bottom, referenceGuide: .safeArea),
+        return [.tip: FloatingPanelLayoutAnchor(fractionalInset: 0.2, edge: .bottom, referenceGuide: .safeArea),
         ]
     }
 }
